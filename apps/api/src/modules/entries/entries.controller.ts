6c3cd607c6c38889e5737aae351128ede802bbb1
Amit Vrubel <amitvrubel@gmail.com>
2026-02-16 20:38:06 +0100

    feat(api): remove entries module (#20)

--- apps/api/src/modules/entries/entries.controller.ts
+++
@@ -1,9 +0,0 @@
-import {Controller, Get} from '@nestjs/common';
-
-@Controller('entries')
-export class EntriesController {
-  @Get()
-  findAll() {
-    return {message: 'Entries endpoint works'};
-  }
-}
